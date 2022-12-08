858e6a1551a33a73d4759268e6721775b6633d22
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-12-08 19:08:38 +0000

    Update - ES Build 0.16 (#2)

--- scripts/build.js
+++ scripts/build.js
@@ -1,21 +1,19 @@
-build({
+buildPlugin({
   entryPoints: [`builds/cdn.js`],
-  outfile: `dist/masonry.min.js`,
-  platform: 'browser',
-  define: { CDN: true },
+  outfile: 'dist/masonry.min.js',
 })
 
-build({
+buildPlugin({
   entryPoints: [`builds/module.js`],
   outfile: `dist/masonry.esm.js`,
   platform: 'neutral',
   mainFields: ['main', 'module'],
 })
 
-function build(options) {
-  options.define || (options.define = {})
-
-  return require('esbuild')
-    .build({ ...options, minify: true, bundle: true })
-    .catch(() => process.exit(1))
+function buildPlugin(buildOptions) {
+  return require('esbuild').buildSync({
+    ...buildOptions,
+    minify: true,
+    bundle: true,
+  })
 }
