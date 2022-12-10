cfb196027c5284adb3276ef5b8e95a751d8752a0
Mark Mead <itsmarkmead@gmail.com>
2022-12-10 10:35:28 +0000

    Update qoutes

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,11 @@
 buildPlugin({
-  entryPoints: [`builds/cdn.js`],
+  entryPoints: ['builds/cdn.js'],
   outfile: 'dist/masonry.min.js',
 })
 
 buildPlugin({
-  entryPoints: [`builds/module.js`],
-  outfile: `dist/masonry.esm.js`,
+  entryPoints: ['builds/module.js'],
+  outfile: 'dist/masonry.esm.js',
   platform: 'neutral',
   mainFields: ['main', 'module'],
 })
